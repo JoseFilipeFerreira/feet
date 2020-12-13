f33ab60085468eae73ab658107301507fd5a577c
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-12-13 21:37:36 +0000

    re

--- Tutorial1/src/main_smoothfilters.m
+++ Tutorial1/src/main_smoothfilters.m
@@ -10,16 +10,16 @@ smooth_img = noisy_img;
 if filtering_domain == "spatial"
     smooth_img = im2double(smooth_img);
     matrix = ones(a);
-    
+
     if smoothing_type == "average"
         for i = 1:a
             for j = 1:a
                 matrix(i,j) = 1/(a*a);
             end
         end
-        
+
         smooth_img = conv2(smooth_img,matrix);
-        
+
         [p3, p4] = size(smooth_img);
         [q1, q2] = size(gray_scale_img);
         q1 = q1 - 1;
@@ -30,33 +30,33 @@ if filtering_domain == "spatial"
         i4_stop = i4_start + q2;
 
         smooth_img = smooth_img(i3_start:i3_stop, i4_start:i4_stop, :);
-        
+
     elseif smoothing_type == "gaussian"
         h = fspecial('gaussian',z,a);
         smooth_img = imfilter(smooth_img, h);
-        
+
     elseif smoothing_type == "median"
         [p,q] = size(matrix);
         smooth_img = medfilt2(smooth_img,[p q]);
     else
         error("Unknow type of smothing");
     end
-    
+
 elseif filtering_domain == "frequency"
     f = im2double(smooth_img);
         [M, N] = size(f);
         P = 2*M;
         Q = 2*N;
         f = padarray(f,[M N],'post');
-            
+
         for i = 1:P
             for j = 1:Q
                 f(i,j) = (-1)^(i+j) * f(i,j);
-            end 
+            end
         end
-            
+
         G = fft2(f);
-            
+
         if smoothing_type == "gaussian"
             H = fspecial('gaussian',[P Q],a);
             H = mat2gray(H);
@@ -64,7 +64,7 @@ elseif filtering_domain == "frequency"
             H = double(zeros(P,Q));
             filter_order = a;
             cutoff = z;
- 
+
             for i=1:P
                 for j=1:Q
                     dist = ((i-P/2)^2 + (j-Q/2)^2)^0.5;
@@ -74,12 +74,12 @@ elseif filtering_domain == "frequency"
         else
             error("Unknow type of smothing");
         end
-        
+
         G = H .* G;
 
         smoothed_image = (ifft2(G));
         smoothed_image = real(smoothed_image);
-        
+
         for i=1:M
             for j=1:N
                 smoothed_image(i,j) = (-1)^(i+j) * smoothed_image(i,j);
@@ -87,10 +87,6 @@ elseif filtering_domain == "frequency"
         end
 
         smooth_img = smoothed_image(1:M, 1:N);
-        
-else
-    error("Unknow type of filtering domain");
-end
 
 imwrite(noisy_img, filepath+"/"+name+"_"+noise_type+"_"+noise_params+".png", "png");
 
