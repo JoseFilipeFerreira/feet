51ce8ba667a2a90420bbf2d9dfade80bf1c880b6
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2021-01-07 23:35:26 +0000

    add feet detection

--- Tutorial2/src/feet_detection.m
+++ Tutorial2/src/feet_detection.m
@@ -1,24 +1,35 @@
-function [] = feet_detection(file_name_rgb, file_name_depth)
+function [left_foot right_foot] = feet_detection(file_name_rgb, file_name_depth)
 
 depth_img = im2gray(imread(file_name_depth));
 rgb_img = im2gray(imread(file_name_rgb));
 
-[sx sy] = size(rgb_img);
 
-figure,imshow(depth_img);
-minx = 205
-maxx = 445
-miny = 60
-maxy = 380
+minx = 205;
+maxx = 445;
+miny = 80;
+maxy = 380;
+
+area_of_interest_depth = depth_img(miny:maxy, minx:maxx);
+
+floor_per_line = specialMax(area_of_interest_depth, 100);
+
+f = polyfit([0:size(floor_per_line)-1], floor_per_line,1);
+
+compensated_floor_per_line = transpose(polyval(f, [0:size(floor_per_line) - 1]));
+
+floor_per_point = repmat(compensated_floor_per_line, 1, maxx - minx + 1);
+
+area_of_interest_depth(area_of_interest_depth > floor_per_point - 100) = 100000;
+area_of_interest_depth(area_of_interest_depth < floor_per_point - 300) = 100000;
+
+area_of_interest_bin = imbinarize(area_of_interest_depth);
+
+figure, imshow(area_of_interest_bin)
+
+figure,imshow(depth_img)
 x = [minx maxx maxx minx minx];
 y = [maxy maxy miny miny maxy];
-
 imshow(rgb_img)
 hold on
 plot(x, y, 'red', 'LineWidth', 4)
-
-floor_per_line = max(depth_img);
-floor_per_line(ones(1,sy), :)
-
 end
-
---
+++ Tutorial2/src/specialMax.m
@@ -0,0 +1,22 @@
+function maxs = specialMax(img,n)
+
+    [rows, columns, ~] = size(img);
+    nMaxs = zeros(rows,n);
+    maxs = zeros(rows,1);
+     
+    for i = 1:rows
+        for j = 1:columns
+            for y = 1:n
+                if img(i,j) > nMaxs(i,y)
+                    for y2 = y:(n-1)
+                        nMaxs(i,y2+1) = nMaxs(i,y2);
+                    end
+                    nMaxs(i,y) = img(i,j);
+                    break
+                end
+            end     
+        end
+    end
+
+    maxs = mean(nMaxs')';
+end
